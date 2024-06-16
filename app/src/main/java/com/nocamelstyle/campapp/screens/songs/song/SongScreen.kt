e7fb93e435d0db6251d157ad2b195f92263618a1
andreykormachenko <andreysxkormachenko@gmail.com>
2024-06-16 22:19:02 +0300

    song details

--- app/src/main/java/com/nocamelstyle/campapp/screens/songs/song/SongScreen.kt
+++ app/src/main/java/com/nocamelstyle/campapp/screens/songs/song/SongScreen.kt
@@ -1,9 +1,37 @@
 package com.nocamelstyle.campapp.screens.songs.song
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.nocamelstyle.campapp.screens.songs.Song
 
 @Composable
 fun SongScreen(song: Song) {
-
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .padding(horizontal = 24.dp)
+            .padding(bottom = 24.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Text(
+            text = song.title,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = song.song
+        )
+    }
 }
